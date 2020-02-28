65bedb3cb59ecec1f9c0e79dd95a22390940d88a
Aldiyar Primbayev <primbayev.96aldi@gmail.com>
2020-02-28 12:04:50 +0600

    Added function welcome_user()

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,6 +1,9 @@
-def main():
-    print('Welcome to the Brain Games!')
+from brain_games.cli import welcome_user
 
 
-if __name__ == '__main__':
-    main()
+def start():
+    return 'Welcome to the Brain Games!\n'
+
+
+print(start())
+print(welcome_user())
