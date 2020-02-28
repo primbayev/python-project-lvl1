fd02b0a5feca7444ba2d007753febe68149260fd
Aldiyar Primbayev <primbayev.96aldi@gmail.com>
2020-02-28 12:24:53 +0600

    Deleted unnecessary function start(), changed function welcome_user()

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,13 +1,9 @@
 from brain_games.cli import welcome_user
 
 
-def start():
-    return 'Welcome to the Brain Games!\n'
-
-
 def main():
-    print(start())
-    print(welcome_user())
+    print('Welcome to the Brain Games!\n')
+    welcome_user()
 
 
 if __name__ == '__main__':
