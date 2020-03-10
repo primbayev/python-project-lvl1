f6957c3b10895ab668df9bfc60a988489e5c2606
Aldiyar Primbayev <primbayev.96aldi@gmail.com>
2020-03-10 17:15:06 +0600

    Renamed variables in brain_calc_game

--- brain_games/games/brain_calc_game.py
+++ brain_games/games/brain_calc_game.py
@@ -6,18 +6,18 @@ def game_process():
     operations = ('+', '-', '*')
     for i in range(0, 3):
         random_operation = operations[i]
-        first_element = randrange(0, 30)
-        second_element = randrange(0, 30)
+        first_number = randrange(0, 30)
+        second_number = randrange(0, 30)
 
         print(
             "Question: {} {} {}".format(
-                first_element, random_operation, second_element
+                first_number, random_operation, second_number
             )
         )
         user_answer = prompt_string('Your answer: ')
 
         correct_answer = eval(
-            '{} {} {}'.format(first_element, random_operation, second_element)
+            '{} {} {}'.format(first_number, random_operation, second_number)
         )
 
         is_answer_correct = check_answer(user_answer, correct_answer)
