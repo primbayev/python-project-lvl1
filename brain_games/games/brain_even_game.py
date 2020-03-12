fa6f140cc5a6b0c695ef91e5a735ad4effaafded
Aldiyar Primbayev <primbayev.96aldi@gmail.com>
2020-03-12 12:20:18 +0600

    Revert "Made common game processes for brain_even and brain_prime"
    This reverts commit 96cddeafe087e9f1675b3af6783d892c73053175.

--- brain_games/games/brain_even_game.py
+++ brain_games/games/brain_even_game.py
@@ -1,7 +1,28 @@
-from brain_games.games.brain_games_common import (
-    common_game_prime_and_even_processes)
+from brain_games import prompt_string, randrange
+from brain_games.games.brain_games_common import initial_welcome, check_answer
+from brain_games.games.brain_games_common import welcome_user, final_message
 
 
 def game_process():
-    task_message = 'Answer "yes" if number even otherwise answer "no".\n'
-    common_game_prime_and_even_processes(task_message, 'check_even_number')
+    initial_welcome()
+    print('Answer "yes" if number even otherwise answer "no".\n')
+    name = welcome_user()
+
+    for i in range(0, 3):
+        random_number = randrange(0, 50)
+        print('Question: {}'.format(random_number))
+
+        user_answer = prompt_string('Your answer: ')
+        correct_answer = check_is_even(random_number)
+
+        is_user_answer_correct = check_answer(user_answer, correct_answer)
+        if not is_user_answer_correct:
+            break
+
+    final_message(name, is_user_answer_correct)
+
+
+def check_is_even(number):
+    if number % 2 == 0:
+        return 'yes'
+    return 'no'
--- brain_games/games/brain_prime_game.py
+++ brain_games/games/brain_prime_game.py
@@ -1,8 +1,33 @@
-from brain_games.games.brain_games_common import (
-    common_game_prime_and_even_processes)
+from brain_games import prompt_string, randrange
+from brain_games.games.brain_games_common import initial_welcome, check_answer
+from brain_games.games.brain_games_common import welcome_user, final_message
 
 
 def game_process():
-    task_message = 'Answer "yes" if given number is prime. \
-Otherwise answer "no".\n'
-    common_game_prime_and_even_processes(task_message, 'check_prime_number')
+    initial_welcome()
+    print('Answer "yes" if given number is prime. Otherwise answer "no".\n')
+    name = welcome_user()
+
+    for i in range(0, 3):
+        random_number = randrange(0, 100)
+        print('Question: {}'.format(random_number))
+
+        user_answer = prompt_string('Your answer: ')
+        correct_answer = check_prime_number(random_number)
+
+        is_user_answer_correct = check_answer(user_answer, correct_answer)
+        if not is_user_answer_correct:
+            break
+
+    final_message(name, is_user_answer_correct)
+
+
+def check_prime_number(number):
+    counter = 0
+    for i in range(2, number):
+        if number % i == 0:
+            counter += 1
+
+    if counter == 0 and number > 1:
+        return 'yes'
+    return 'no'
