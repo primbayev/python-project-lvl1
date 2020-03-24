6d5b534c672f00b0dbc3b48c3814e348acbd0df4
Aldiyar Primbayev <primbayev.96aldi@gmail.com>
2020-03-24 16:39:14 +0600

    Changed function check_is_prime to is_prime

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -8,16 +8,15 @@ def generate_round():
     random_number = randrange(0, 100)
     print('Question: {}'.format(random_number))
 
-    correct_answer = check_is_prime(random_number)
-    return correct_answer
+    return "yes" if is_prime(random_number) else "no"
 
 
-def check_is_prime(number):
+def is_prime(number):
     counter = 0
     for i in range(2, number):
         if number % i == 0:
             counter += 1
 
     if counter == 0 and number > 1:
-        return 'yes'
-    return 'no'
+        return True
+    return False
