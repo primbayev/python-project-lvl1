e0ea9172962d754fe1c9d77bac937bc4ef6063be
Aldiyar Primbayev <primbayev.96aldi@gmail.com>
2020-03-29 13:04:49 +0600

    Changed how prime number is checked

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -13,11 +13,9 @@ def generate_round():
 
 
 def is_prime(number):
-    counter = 0
-    for i in range(2, number):
-        if number % i == 0:
-            counter += 1
-
-    if counter == 0 and number > 1:
+    if number > 1:
+        for i in range(2, number // 2 + 1):
+            if number % i == 0:
+                return False
         return True
     return False
